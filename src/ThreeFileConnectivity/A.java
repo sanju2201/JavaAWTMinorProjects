13fe0f96b5891e52798f09de63b899ba1bb45d47
SanjuManna <sanjumanna2201@gmail.com>
2022-10-31 12:00:19 +0530

    commit

---
+++ src/ThreeFileConnectivity/A.java
@@ -0,0 +1,24 @@
+package ThreeFileConnectivity;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class A extends Frame implements ActionListener {
+
+
+    A(){
+        setLayout(null);
+
+
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public static void main(String[] args) {
+        A objA = new A();
+        objA.setSize(400,400);
+        objA.setVisible(true);
+
+    }
+}
---
+++ src/ThreeFileConnectivity/C.java
@@ -0,0 +1,4 @@
+package ThreeFileConnectivity;
+
+public class C {
+}
